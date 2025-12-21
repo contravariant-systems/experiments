de1939780b40a7ec54b796040ead95aa024478e8
Harish Narayanan <hnarayanan@gmail.com>
2025-12-21 22:49:06 +0000

    Add experimentation for day 2

--- sho2.py
+++ sho2.py
@@ -5,6 +5,8 @@ import jax
 import jax.numpy as jnp
 from jax.lax import scan
 from jax import vmap, grad, jit
+import optax
+from optax import transforms
 import matplotlib.pyplot as plt
 
 
@@ -116,21 +118,90 @@ traj_euler = integrate_euler(state_0, n_steps, dt, params)
 
 plt.plot(traj_euler[:, 0])
 plt.ylabel('Position over time (Forward Euler)')
-plt.show()
+# plt.show()
 
 traj_rk4 = integrate_rk4(state_0, n_steps, dt, params)
 
 plt.plot(traj_rk4[:, 0])
 plt.ylabel('Position over time (Runge-Kutta 4)')
-plt.show()
+# plt.show()
 
 energy_evolution_euler = vmap(total_energy, (0, None))(traj_euler, params)
 plt.plot(energy_evolution_euler)
 plt.ylabel('Energy over time (Forward Euler)')
-plt.show()
+# plt.show()
 
 energy_evolution_rk4 = vmap(total_energy, (0, None))(traj_rk4, params)
 plt.plot(energy_evolution_rk4)
 plt.ylabel('Energy over time (Runge-Kutta 4)')
-plt.show()
+# plt.show()
 
+# Now that we've established that RK4 is very good, let's move onto
+# the inverse problem. i.e. we assume the parameters of the model are
+# unknown and estimate them from trajectories.
+
+k_true = 2.0
+
+params = {
+    "m": 1.0,
+    "k": k_true,
+}
+state_0 = jnp.array([1.0, 0.0])
+n_steps = 1000
+dt = 0.01
+traj_true = integrate_rk4(state_0, n_steps, dt, params)
+
+key = jax.random.PRNGKey(42)
+noise_scale = 0.05
+noise = noise_scale * jax.random.normal(key, traj_true.shape)
+traj_observed = traj_true + noise
+
+# In the future, we can think of a more sophisticated loss, possibly
+# based on the following and the definition of the Hamiltonion, total
+# energy itself to scale them and properly dimensionally combine them,
+#
+# L_posn = jnp.linalg.norm(traj_observed[:, 0] - traj_guess[:, 0], ord=2)
+# L_mom = jnp.linalg.norm(traj_observed[:, 1] - traj_guess[:, 1], ord=2)
+#
+# but for now we just go with something simple.
+
+def loss(traj_observed, state_0, n_steps, dt, params_guess):
+    traj_guess = integrate_rk4(state_0, n_steps, dt, params_guess)
+    return jnp.linalg.norm(traj_observed - traj_guess)**2
+
+# print(grad(loss, argnums=[4])(traj_observed, state_0, n_steps, dt, params_guess))
+
+k_guess = 1.0
+eta = 1.e-5
+
+print("Manual optimisation:")
+
+for i in range(100):
+    if i % 10 == 0:
+        print(k_guess)
+    params_guess = {
+        "m": 1.0,
+        "k": k_guess
+    }
+    gradL = grad(loss, argnums=[4])(traj_observed, state_0, n_steps, dt, params_guess)[0]['k']
+    k_guess = k_guess - eta*gradL
+
+print("Optax (Adam) optimisation:")
+
+eta = 1e-1
+freeze = transforms.freeze
+selective_transform = transforms.selective_transform
+mask = {"m": False, "k": False}
+
+optimizer = optax.chain(optax.adam(eta), freeze(mask))
+
+params_guess = {"m": 1.0, "k": 1.0}
+opt_state = optimizer.init(params_guess)
+
+for i in range(100):
+    grads = grad(loss, argnums=[4])(traj_observed, state_0, n_steps, dt, params_guess)[0]
+    updates, opt_state = optimizer.update(grads, opt_state)
+    params_guess = optax.apply_updates(params_guess, updates)
+
+    if i % 10 == 0:
+        print(f"k={params_guess['k']:.6f}, m={params_guess['m']:.6f}")
