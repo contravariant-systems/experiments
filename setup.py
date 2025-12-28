f5c3c12c13f3d4a7c9849dffe4e083fe291a6c41
Harish Narayanan <hnarayanan@gmail.com>
2025-12-28 15:53:51 +0000

    Pass files through source formatter

--- setup.py
+++ setup.py
@@ -2,8 +2,7 @@ from setuptools import setup, find_packages
 
 with open("requirements.txt") as f:
     requirements = [
-        line.strip() for line in f
-        if line.strip() and not line.startswith("#")
+        line.strip() for line in f if line.strip() and not line.startswith("#")
     ]
 
 setup(
